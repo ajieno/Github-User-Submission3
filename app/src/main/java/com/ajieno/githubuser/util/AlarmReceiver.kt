ca11fb24dbcc7ef34ddf3e745c72d170bc32c4b5
ajieno <ajie.nofrizan@gmail.com>
2021-03-31 09:49:41 +0700

    Alarm Updated

--- app/src/main/java/com/ajieno/githubuser/util/AlarmReceiver.kt
+++ app/src/main/java/com/ajieno/githubuser/util/AlarmReceiver.kt
@@ -1,38 +1,74 @@
 package com.ajieno.githubuser.util
 
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import com.ajieno.githubuser.R
+import com.ajieno.githubuser.view.MainActivity
+import java.util.*
 
 class AlarmReceiver : BroadcastReceiver() {
 
     companion object {
-        const val TYPE_ONE_TIME = "OneTimeAlarm"
         const val TYPE_REPEATING = "RepeatingAlarm"
         const val EXTRA_MESSAGE = "message"
         const val EXTRA_TYPE = "type"
 
         private const val ID_REPEATING = 101
+        private const val TIME_REPEATING = "09:00"
 
-        private const val DATE_FORMAT = "yyyy-MM-dd"
-        private const val TIME_FORMAT = "HH:mm"
     }
 
     override fun onReceive(context: Context, intent: Intent) {
         val type = intent.getStringExtra(EXTRA_TYPE)
         val message = intent.getStringExtra(EXTRA_MESSAGE)
-        val title = if (type.equals(TYPE_ONE_TIME, ignoreCase = true)) TYPE_ONE_TIME else TYPE_REPEATING
+        val title = "Find more friend in Github User"
 
-        showToast(context, title, message)
+        showNotif(context, message)
     }
 
-    private fun showToast(context: Context, title: String, message: String?) {
-        Toast.makeText(context, "$title : $message", Toast.LENGTH_LONG).show()
+    private fun showNotif(context: Context, message: String?) {
+        val channelID = "ajieno"
+        val channelName = "daily notif"
+
+        val intent = Intent(context, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+                context,0,
+                intent,PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val notificationManagerCompat =
+                context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val builder = NotificationCompat.Builder(context, channelID)
+                .setContentTitle("Daily Reminder")
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                    channelID,
+                    channelName,
+                    NotificationManager.IMPORTANCE_DEFAULT
+            )
+            builder.setChannelId(channelID)
+            notificationManagerCompat.createNotificationChannel(channel)
+        }
+
+        val notification = builder.build()
+        notificationManagerCompat.notify(100, notification)
     }
 
     fun setRepeatingAlarm(context: Context, type: String, time: String, message: String) {
-        if (isDateInvalid(time, TIME_FORMAT)) return
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val intent = Intent(context, AlarmReceiver::class.java)
         intent.putExtra(EXTRA_MESSAGE, message)
@@ -42,8 +78,19 @@ class AlarmReceiver : BroadcastReceiver() {
         calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(timeArray[0]))
         calendar.set(Calendar.MINUTE, Integer.parseInt(timeArray[1]))
         calendar.set(Calendar.SECOND, 0)
+
         val pendingIntent = PendingIntent.getBroadcast(context, ID_REPEATING, intent, 0)
         alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent)
         Toast.makeText(context, "Repeating alarm set up", Toast.LENGTH_SHORT).show()
     }
+
+    fun cancelAlarm(context: Context) {
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, AlarmReceiver::class.java)
+        val requestCode = ID_REPEATING
+        val pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, 0)
+        pendingIntent.cancel()
+        alarmManager.cancel(pendingIntent)
+    }
+
 }
--- app/src/main/java/com/ajieno/githubuser/view/SettingActivity.kt
+++ app/src/main/java/com/ajieno/githubuser/view/SettingActivity.kt
@@ -1,14 +1,54 @@
 package com.ajieno.githubuser.view
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.ajieno.githubuser.R
+import com.ajieno.githubuser.util.AlarmReceiver
+import kotlinx.android.synthetic.main.activity_setting.*
 
 class SettingActivity : AppCompatActivity() {
+
+    private lateinit var alarmReceiver: AlarmReceiver
+    private lateinit var sharedPreferences: SharedPreferences
+
+    companion object{
+        const val SAVE_ALARM = "set alarm"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+        supportActionBar?.title = "Setting"
 
-        supportActionBar?.hide()
+        alarmReceiver = AlarmReceiver()
+        sharedPreferences = getSharedPreferences("Setting", Context.MODE_PRIVATE)
+
+        setSwitch()
+
+        sw_alarm.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked){
+//                alarmReceiver.setRepeatingAlarm(
+//                        this,AlarmReceiver.EXTRA_TYPE,
+//                        "09:00",
+//                        "Let's connect with another")
+                sharedPreferences.edit().putBoolean("alarm",true).apply()
+                Toast.makeText(this, "Alarm Activate", Toast.LENGTH_SHORT).show()
+            }else{
+                alarmReceiver.cancelAlarm(this)
+                sharedPreferences.edit().putBoolean("alarm", false).apply()
+                Toast.makeText(this, "Alarm Deactive", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun setSwitch() {
+        val value = sharedPreferences.getBoolean("alarm",true)
+        if (value == true){
+            sw_alarm.isChecked = true
+        }
     }
 }
